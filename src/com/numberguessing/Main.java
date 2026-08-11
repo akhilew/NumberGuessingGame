81aca94147b35175ab548cde7f3b403c3b072736
akhilew <akhiln.connect@gmail.com>
2026-08-11 17:56:57 +0530

    Add random number and user input

--- src/com/numberguessing/Main.java
+++ src/com/numberguessing/Main.java
@@ -1,10 +1,25 @@
 package com.numberguessing;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Number Guessing Game");
 
+        Random random = new Random();
+        int secretNumber = random.nextInt(100) + 1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your guess: ");
+        int guess = input.nextInt();
+
+        System.out.println("Your guess: " + guess);
+        System.out.println("The secret number is: " + secretNumber);
+
+        input.close();
     }
 }
